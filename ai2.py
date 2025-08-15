7e69d638239f31383897fbeb8f098e1132da90d4
Dohyun Kim <76870940+dohyun06@users.noreply.github.com>
2025-08-15 16:41:22 +0900

    feat: make ai2.py that is copy ai2-1.ipynb

---
+++ ai2.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+data_dir = './dataset/step2'
+IMG_SIZE = 224 
+BATCH_SIZE = 32
+EPOCHS = 15  
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    data_dir, 
+    validation_split=0.2, 
+    subset="training", 
+    seed=123, 
+    image_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE
+)  
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_dataset.class_names
+print("Names of class:", class_names)
+num_classes = len(class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip('horizontal'),
+    tf.keras.layers.RandomRotation(0.2),
+])
+
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    include_top=False,
+    weights='imagenet'
+)
+
+base_model.trainable = False
+
+inputs = tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+x = data_augmentation(inputs)
+x = base_model(x, training=False) 
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+
+outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+
+model = tf.keras.Model(inputs, outputs)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-4),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+
+print("model:")
+model.summary()
+
+print("Start to learn")
+history = model.fit(
+    train_dataset,
+    validation_data=validation_dataset,
+    epochs=EPOCHS
+)
+print("Finish")
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+import glob
+
+image_paths = glob.glob(os.path.join('./dataset/step3/*.jpg'))
+
+for test_image_path in image_paths:
+    img = tf.keras.utils.load_img(
+        test_image_path, target_size=(IMG_SIZE, IMG_SIZE)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array, verbose=0)
+    score = tf.nn.softmax(predictions[0])
+
+    file_name = os.path.basename(test_image_path)
+    predicted_class = class_names[np.argmax(score)]
+    confidence = 100 * np.max(score)
+
+    print(f"File: {file_name} -> Class Prediction: '{predicted_class}' ({confidence:.2f}%)")
+    
+print('Finish to test')
